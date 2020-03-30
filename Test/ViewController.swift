10410a1b2d6049354a17553442ff827fc318e7fd
macocz <maciek.oczos@gmail.com>
2020-03-30 21:49:11 +0200

    first commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Maciej Oczoś on 30/03/2020.
+//  Copyright © 2020 Maciej Oczoś. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
